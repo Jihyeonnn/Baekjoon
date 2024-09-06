b706f024030768e36400cabdec05813de42f2246
Jihyeon Song <101876224+Jihyeonnn@users.noreply.github.com>
2024-09-06 15:01:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1200 ms, Memory: 70088 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,9 @@
+import sys
+T = int(sys.stdin.readline())
+result = []
+for _ in range(T):
+    a, b = map(int, sys.stdin.readline().split())
+    result.append(a+b)
+
+for i in range(T):
+    print(result[i])
